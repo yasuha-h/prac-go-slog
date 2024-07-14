6d5545c3633d736b438b471f1de9c72d63cddc02
yasuha-h <kurukami135@gmail.com>
2024-07-14 13:49:24 +0900

    用途別に出力先を分ける

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"log/slog"
 	"os"
 
@@ -10,24 +11,46 @@ import (
 )
 
 func main() {
-	// カスタムロガーを作成
-	h := logger.NewCustomLogHandler(slog.NewJSONHandler(os.Stdout,
-		&slog.HandlerOptions{
-			Level: slog.LevelInfo,
-		},
-	))
-	// カスタムロガーをセット
-	slog.SetDefault(slog.New(h))
-
 	// トレースIDをUUIDで生成
 	traceId := uuid.New().String()
 	// トレースIDをコンテキストにセット
 	ctx := logger.WithTraceId(context.Background(), traceId)
 
+	// accessLog
+	accessLog, err := os.OpenFile("./logs/access.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// アプリケーション用のカスタムロガーを作成
+	accessLogHandler := logger.NewCustomLogHandler(
+		slog.NewJSONHandler(accessLog, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	slog.New(accessLogHandler).DebugContext(ctx, "access log")
+
+	// appLog
+	appLog, err := os.OpenFile("./logs/app.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// アプリケーション用のカスタムロガーを作成
+	appLogHandler := logger.NewCustomLogHandler(
+		slog.NewJSONHandler(
+			appLog,
+			&slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			},
+		))
+	// カスタムロガーをセット
+	slog.SetDefault(slog.New(appLogHandler))
+
 	// ログ出力 トレースIDをコンテキスト経由で共通で出力できているか確認
-	slog.InfoContext(ctx, "Messageeeeee")
-	slog.InfoContext(ctx, "Messageeeeee")
-	slog.InfoContext(ctx, "Messageeeeee")
+	slog.InfoContext(ctx, "app1")
+	slog.InfoContext(ctx, "app2")
+	slog.InfoContext(ctx, "app3")
 	// コンテキストを使わないとトレースIDが出力されない
-	slog.Info("Messageeeeee")
+	slog.Info("app4")
+
+	// accessLogとLogLevelが異なるため出力されない
+	slog.Debug("Not outputed.")
 }
